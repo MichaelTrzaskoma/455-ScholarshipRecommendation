573dc9a5b5db37b07d1f2dbaa04a020a6b8c1f61
Hui (Henry) Chen <hhc879099766@gmail.com>
2021-03-18 22:35:59 -0400

    Major frontend build change!
    Change: update all npm packages and its dependencies; move logo to 'assets' folder; deleted uncessary folders or files

--- frontend/components/AccScreen.js
+++ frontend/components/AccScreen.js
@@ -1,170 +1,193 @@
-// user did not sign in yet
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+} from 'react-native';
+import MaterialCommunityIconsIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
+import OcticonsIcon from 'react-native-vector-icons/Octicons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
-import React, { Component, useState } from "react";
-import { Button, View, Text, StyleSheet, TouchableOpacity } from "react-native";
-
-import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
-import EntypoIcon from "react-native-vector-icons/Entypo";
-import MaterialIconsIcon from "react-native-vector-icons/MaterialIcons";
-import "react-native-gesture-handler";
-import { useNavigation } from "@react-navigation/native";
-import { withNavigation } from "react-navigation";
-
-// import GlobalState from "../experiments/GlobalState";
 import AddProfile from "../ui/AddProfile";
 
-export default function AccScreen({ usr_info }) {
-  // init things for the log in
-  
+export default function AccountScreen({ usrInfo }) {
 
-  // console.log("Acc Screen: " + usr_info.email);
-  // const navigation = navigation;
-  const user_info = usr_info;
-  return (
-    <View style={styles.container}>
-      <View style={styles.rect}>
-        <View style={styles.grp1}>
-          <View style={styles.iconRow}>
-            <FontAwesomeIcon
-              name="user-o"
-              style={styles.icon}
-            ></FontAwesomeIcon>
-            <View style={styles.fullNameHereColumn}>
-              <Text style={styles.fullNameHere}>{user_info.full_name}</Text>
-              <Text style={styles.email}>Email: {user_info.email}</Text>
+    // console.log(usrInfo)
+    // const user_info = usrInfoObj;
+    return (
+        <View style={styles.container}>
+            <View style={styles.content_container}>
+                <View style={styles.usrInfoContainer}>
+                    <View style={styles.usrIconRow}>
+                        <MaterialCommunityIconsIcon
+                            name="account-outline"
+                            style={styles.usrIcon}></MaterialCommunityIconsIcon>
+                        <View style={styles.nameTxtColumn}>
+                            <Text style={styles.nameTxt}>Name:</Text>
+                            <Text style={styles.emailTxt}>Email:</Text>
+                        </View>
+                        <View style={styles.namePlaceHolderColumn}>
+                            <Text style={styles.namePlaceHolder}>{usrInfo.full_name}</Text>
+                            <Text style={styles.usrEmailTxt_display}>{usrInfo.email}</Text>
+                        </View>
+                    </View>
+                </View>
+
+                <View style={styles.accBtnGrp}>
+                    <AddProfile emailer={usrInfo.email} />
+
+                    <TouchableOpacity style={styles.button2}>
+                        <View style={styles.bookmarksIconRow}>
+                            <OcticonsIcon
+                                name="bookmark"
+                                style={styles.bookmarksIcon}></OcticonsIcon>
+                            <Text style={styles.bookmarksTxt}>Bookmarks</Text>
+                        </View>
+                        <View style={styles.iconFiller}></View>
+                        <EntypoIcon
+                            name="chevron-small-right"
+                            style={styles.optBtn_arrowRightICON}></EntypoIcon>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.button3}>
+                        <View style={styles.signOutIconRow}>
+                            <FontAwesomeIcon
+                                name="sign-out"
+                                style={styles.signOutIcon}></FontAwesomeIcon>
+                            <Text style={styles.signOut2}>Sign Out</Text>
+                        </View>
+                        <View style={styles.iconFiller}></View>
+                        <EntypoIcon
+                            name="chevron-small-right"
+                            style={styles.optBtn_arrowRightICON}></EntypoIcon>
+                    </TouchableOpacity>
+                </View>
             </View>
-          </View>
         </View>
-
-        <AddProfile emailer={user_info.email} />
-
-        <TouchableOpacity
-          // onPress={() => navigation.navigate("Untitled")}
-          style={styles.grp3}
-        >
-          <View style={styles.icon3Row}>
-            <FontAwesomeIcon
-              name="sign-out"
-              style={styles.icon3}
-            ></FontAwesomeIcon>
-            <Text style={styles.signOut}>Sign Out</Text>
-          </View>
-          <View style={styles.icon3RowFiller}></View>
-          <MaterialIconsIcon
-            name="chevron-right"
-            style={styles.icon4}
-          ></MaterialIconsIcon>
-        </TouchableOpacity>
-      </View>
-      {/* {getAPIINFO} */}
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    fontSize: 20,
-  },
-  container: {
-    flex: 1,
-  },
-  rect: {
-    backgroundColor: "#E6E6E6",
-    flex: 1,
-  },
-  grp1: {
-    height: 125,
-    backgroundColor: "rgba(255,255,255,1)",
-    marginTop: 57,
-  },
-  icon: {
-    color: "rgba(128,128,128,1)",
-    fontSize: 60,
-  },
-  fullNameHere: {
-    color: "#121212",
-    fontSize: 16,
-  },
-  email: {
-    color: "rgba(78,78,78,1)",
-    fontSize: 12,
-    marginTop: 13,
-  },
-  fullNameHereColumn: {
-    width: 159,
-    marginLeft: 29,
-    marginTop: 8,
-    marginBottom: 6,
-  },
-  iconRow: {
-    height: 60,
-    flexDirection: "row",
-    marginTop: 32,
-    marginLeft: 21,
-    marginRight: 99,
-  },
-  grp2: {
-    height: 50,
-    backgroundColor: "rgba(255,255,255,1)",
-    flexDirection: "row",
-    marginTop: 33,
-  },
-  icon2: {
-    color: "rgba(65,117,5,1)",
-    fontSize: 25,
-  },
-  addProfileDetail: {
-    color: "#121212",
-    fontSize: 15,
-    marginLeft: 22,
-    marginTop: 4,
-  },
-  icon2Row: {
-    height: 27,
-    flexDirection: "row",
-    marginLeft: 17,
-    marginTop: 12,
-  },
-  icon2RowFiller: {
-    flex: 1,
-    flexDirection: "row",
-  },
-  icon5: {
-    color: "rgba(174,174,174,1)",
-    fontSize: 25,
-    marginRight: 10,
-    marginTop: 13,
-  },
-  grp3: {
-    height: 50,
-    backgroundColor: "rgba(255,255,255,1)",
-    flexDirection: "row",
-    marginTop: 27,
-  },
-  icon3: {
-    color: "rgba(208,2,27,1)",
-    fontSize: 25,
-  },
-  signOut: {
-    color: "#121212",
-    fontSize: 15,
-    marginLeft: 24,
-    marginTop: 3,
-  },
-  icon3Row: {
-    height: 25,
-    flexDirection: "row",
-    marginLeft: 17,
-    marginTop: 13,
-  },
-  icon3RowFiller: {
-    flex: 1,
-    flexDirection: "row",
-  },
-  icon4: {
-    color: "rgba(174,174,174,1)",
-    fontSize: 25,
-    marginRight: 10,
-    marginTop: 13,
-  },
+    container: {
+        flex: 1,
+    },
+    content_container: {
+        backgroundColor: "rgba(255,255,255,1)",
+        flex: 1,
+    },
+    usrInfoContainer: {
+        height: 113,
+        backgroundColor: '#e6e6e6',
+        marginTop: 51,
+    },
+    usrIcon: {
+        color: 'rgba(155,155,155,1)',
+        fontSize: 80,
+    },
+    nameTxt: {
+        color: '#121212',
+        fontSize: 16,
+    },
+    emailTxt: {
+        color: '#121212',
+        fontSize: 16,
+        marginTop: 11,
+    },
+    nameTxtColumn: {
+        width: 47,
+        marginLeft: 18,
+        marginTop: 17,
+        marginBottom: 19,
+    },
+    namePlaceHolder: {
+        color: '#121212',
+        height: 20,
+        width: 153,
+        fontSize: 16,
+        marginLeft: 2,
+    },
+    usrEmailTxt_display: {
+        color: '#121212',
+        height: 20,
+        width: 153,
+        fontSize: 16,
+        marginTop: 10,
+    },
+    namePlaceHolderColumn: {
+        width: 155,
+        marginLeft: 7,
+        marginTop: 17,
+        marginBottom: 20,
+    },
+    usrIconRow: {
+        height: 87,
+        flexDirection: 'row',
+        marginTop: 13,
+        marginLeft: 15,
+        marginRight: 38,
+    },
+    accBtnGrp: {
+        height: 224,
+        marginTop: 58,
+    },
+
+    optBtn_arrowRightICON: {
+        color: 'rgba(155,155,155,1)',
+        fontSize: 40,
+        marginRight: 7,
+        alignSelf: 'center',
+    },
+
+    button2: {
+        height: 50,
+        backgroundColor: 'rgba(230, 230, 230,1)',
+        flexDirection: 'row',
+        marginTop: 14,
+    },
+    bookmarksIcon: {
+        color: 'rgba(48,132,188,1)',
+        fontSize: 30,
+    },
+    bookmarksTxt: {
+        color: '#121212',
+        fontSize: 16,
+        marginLeft: 15,
+        marginTop: 7,
+    },
+    bookmarksIconRow: {
+        height: 33,
+        flexDirection: 'row',
+        marginLeft: 15,
+        marginTop: 9,
+    },
+    iconFiller: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+
+    button3: {
+        height: 50,
+        backgroundColor: 'rgba(230, 230, 230,1)',
+        flexDirection: 'row',
+        marginTop: 59,
+    },
+    signOutIcon: {
+        color: 'rgba(236,78,96,1)',
+        fontSize: 35,
+    },
+    signOut2: {
+
+        color: '#121212',
+        fontSize: 16,
+        marginLeft: 18,
+        marginTop: 8,
+    },
+    signOutIconRow: {
+        height: 35,
+        flexDirection: 'row',
+        marginLeft: 15,
+        marginTop: 8,
+    },
 });
